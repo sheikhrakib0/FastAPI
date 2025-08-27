18fc5df25e7eb3665a6943756dd1c03775a6ec75
sheikhrakib0 <rakibrakib0179@gmai.com>
2025-08-27 16:02:54 +0600

    A patient API integrated

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
+# to laod the json file 
+def load_data():
+    with open('patient.json', 'r') as f:
+        data = json.load(f)
+    return data
+
 @app.get("/")
 def hello():
-    print("hello world")
+    return {"message": "Patient management system"}
+
+
+@app.get('/about')
+def about():
+    return {"message": "A fully functional API to manage patients information"}
+
+@app.get('/view')
+def view():
+    data = load_data()
+
+    return data
+
+
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id:str):
+    #first loading all data 
+    data = load_data()
+
+    for patient in data:
+        if patient_id in patient['id']:
+            return patient
+    return {'error': 'This patient information is not available to this database'}
